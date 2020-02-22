4069805291f77489c844eef5bae10df29fd3f48e
Vibin Daniel <vibindaniel@outlook.com>
2020-02-22 04:04:26 -0500

    refactored the scripts

--- copydvd.bat
+++ copydvd.bat
@@ -1,18 +1,3 @@
 @echo off
-:ask_for_folder_name
-SET /P _name="Enter folder name: "
-IF "%_name%" equ "" GOTO :ask_for_folder_name
-
-:ask_for_drive
-echo Choose a dvd drive
-echo g
-echo h
-SET /P _drive="Your choice is? "
-IF not "%_drive%" == "g" (
-    if not "%_drive%" == "h" (
-        echo Choice is invalid
-        GOTO :ask_for_drive
-    )
-)
-
-pwsh copydvd.ps1 "%_name%" "%_drive%"
+pwsh %~dp0copydvd.ps1
+exit
--- copydvd.ps1
+++ copydvd.ps1
@@ -1,19 +1,82 @@
-param (
-    [Parameter(Mandatory = $true, Position = 0)]
-    [string]$name = $(throw "-name is required";),
-    [Parameter(Mandatory = $false, Position = 1)]
-    [string]$drives = "G,H"
-)
+function Get-CDDrives {
+    $drives = ([string](wmic CDROM where "MediaType!='unknown' and MediaLoaded='True'" get Drive)).
+                ToLowerInvariant();
+    $drives = [regex]::Replace($drives, "\s+", " ");
+    return $drives.Split(" ") | Select-Object -Skip 1 | Where-Object { $_ -ne "" } | ForEach-Object {
+        $_.SubString(0, 1);
+    };
+}
 
-$drives.Split(",") | ForEach-Object {
-    if(Test-Path -Path "${_}:\VIDEO_TS") {
-        $drive = $_
-        # Copy-Item "${_}:\VIDEO_TS\*" -Destination ".\${name}" -Recurse
-        robocopy "${drive}:\VIDEO_TS" ".\${name}" /s /e
+function ValidateName {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNull()]
+        [string]
+        $Name
+    )
+}
 
-        $sh = New-Object -ComObject "Shell.Application"
-        $sh.Namespace(17).Items() |
-            Where-Object { $_.Type -eq "CD Drive" -and $_.Path -ieq "${drive}:\" } |
-            ForEach-Object { $_.InvokeVerb("Eject") }
+function ValidateDrive {
+    Param (
+        [Parameter(Mandatory=$true, Position = 1)]
+        [ValidateScript({ (Get-CDDrives) -contains $_ })]
+        [string]
+        $Drive
+    )
+}
+
+function ValidateNameResult {
+    Param (
+        $Name
+    )
+    try {
+        ValidateName($Name);
+        return $true;
+    } catch {
+        Write-Error $_;
+        return $false;
     }
+}
+
+function ValidateDriveResult {
+    Param (
+        $Drive
+    )
+    try {
+        ValidateDrive($Drive);
+        return $true;
+    } catch {
+        Write-Error $_;
+        return $false;
+    }
+}
+
+$Name = "";
+$Drive = "";
+Do{
+    $Name = $(Read-Host -Prompt "Enter Destination Folder Name");
+} while(-Not (ValidateNameResult($Name)))
+
+Do{
+    $Drive = $(Read-Host -Prompt `
+    "
+List of available loaded drive choices
+$((Get-CDDrives) | ForEach-Object { $i = 0 } {
+    $i++;
+    if($i -eq 1) {
+        " ${i}. ${_}`r`n";
+    } else {
+        "${i}. ${_}`r`n";
+    }
+})
+Enter your drive choice");
+} while(-Not (ValidateDriveResult($Drive)))
+
+if(Test-Path -Path "${Drive}:\VIDEO_TS") {
+    robocopy "${Drive}:\VIDEO_TS" ".\${Name}" /s /e;
+
+    $sh = New-Object -ComObject "Shell.Application";
+    $sh.Namespace(17).Items() |
+        Where-Object { $_.Type -eq "CD Drive" -and $_.Path -ieq "${Drive}:\" } |
+        ForEach-Object { $_.InvokeVerb("Eject") }
 }
